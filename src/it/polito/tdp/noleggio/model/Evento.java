f124ab0fab338fda540311cb52165220964ae905
elisabettaravelli <elisabettaravelli@172.22.57.158>
2019-05-20 14:25:42 +0200

    Soluzione

---
+++ src/it/polito/tdp/noleggio/model/Evento.java
@@ -0,0 +1,35 @@
+package it.polito.tdp.noleggio.model;
+
+import java.time.LocalTime;
+
+public class Evento implements Comparable<Evento>{
+	
+	public enum TipoEvento{
+		CLIENTE_ARRIVA,
+		AUTO_RESTITUITA
+	}
+	
+	private LocalTime tempo;
+	private TipoEvento tipo;
+	
+	public Evento(LocalTime tempo, TipoEvento tipo) {
+		super();
+		this.tempo = tempo;
+		this.tipo = tipo;
+	}
+	
+	@Override
+	public int compareTo(Evento other) {
+		return this.tempo.compareTo(other.tempo);
+	}
+
+	public LocalTime getTempo() {
+		return tempo;
+	}
+
+	public TipoEvento getTipo() {
+		return tipo;
+	}
+	
+	
+}
